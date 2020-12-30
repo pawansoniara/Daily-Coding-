d2da410c2a2af3f5b27aa4c543ddeff12e1c8659
Pawan <pawan@bursys.com>
2020-12-30 11:21:45 +0530

    app-86

--- app-60.js
+++ app-60.js
@@ -1,4 +1,16 @@
 
+/**
+ * This problem was asked by Facebook.
+
+Given a multiset of integers, return whether it can be partitioned into two subsets whose sums are the same.
+
+For example, given the multiset {15, 5, 20, 10, 35, 15, 10}, it would return true, since we can split it up into {15, 5, 10, 15, 10} and {20, 35}, which both add up to 55.
+
+Given the multiset {15, 5, 20, 10, 35}, it would return false, since we can't split it up into two subsets that add up to the same sum.
+ * 
+ */
+
+
 const abc = [45, 65,  65, 1, 1, 31.5, 1, 1,45, 54, 21,45, 54, 20.5, 32, 45]
 const total = abc.reduce((p, c) => p + c)
 
